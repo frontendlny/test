7fa9e929ee80c0ecb236f1421e9615828bd760b8
lny <1604183820@qq.com>
2021-01-17 20:41:56 +0800

    ok

--- koa/readme.md
+++ koa/readme.md
@@ -2,5 +2,10 @@ npm init -y
 cnpm i -S koa koa-router
 
 ## express koa
-express
-koa
+express 异步回调多层嵌套
+koa  异步同步化  对promise支持较好
+
+"洋葱皮": 从外到里面一层一层包裹,
+调用的过程: 从左边最外层--->最里面--->从右边最外层出来
+
+http://127.0.0.1:3000/api/test
--- koa/simple.js
+++ koa/simple.js
@@ -4,12 +4,25 @@ const Router = require('koa-router');
 const app = new koa();
 const router = Router();
 
+// 拦截器
+app.use(async(ctx, next) => {
+  console.log('--------拦截器开始----------');
+  await next();
+  console.log('--------拦截器结束----------');
+});
+app.use(async(ctx, next) => {
+  console.log('--------拦截器2开始----------');
+  await next();
+  console.log('--------拦截器2结束----------');
+});
+
 router.get('/api/test', (ctx, next) => {
+  console.log('/api/test');
   var json = { name: 'jack' };
   ctx.body = JSON.stringify(json);
-})
+});
 app.use(router.routes());
 
-app.listen(3333, () => {
-  console.log("服务器已启动");
-})
+app.listen(3000, () => {
+  console.log('服务器已启动, 端口3000');
+});
